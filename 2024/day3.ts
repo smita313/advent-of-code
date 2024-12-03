43ca3f1e51dd9d19060e59813f85108ece545a30
Smita Patankar <smita.patankar@letshighlight.com>
2024-12-03 13:03:22 -0500

    Day 3 Part 2

--- 2024/day3.ts
+++ 2024/day3.ts
@@ -1,15 +1,53 @@
 import { readFileSync } from "fs";
 
+const sumValidProducts = (str: string) => {
+  return (
+    str
+      .match(/mul\(\d+,\d+\)/g)
+      ?.map((mul) => {
+        const nums = mul.match(/\d+/g);
+        return parseInt(nums?.at(0) ?? "1") * parseInt(nums?.at(1) ?? "1");
+      })
+      .reduce((sum, current) => sum + current, 0) ?? 0
+  );
+};
+
 const part1 = () => {
   const fileStr = readFileSync("./inputs/day3.txt").toString();
 
-  return fileStr
-    .match(/mul\(\d+,\d+\)/g)
-    ?.map((mul) => {
-      const nums = mul.match(/\d+/g);
-      return parseInt(nums?.at(0) ?? "1") * parseInt(nums?.at(1) ?? "1");
-    })
-    .reduce((sum, current) => sum + current, 0);
+  return sumValidProducts(fileStr);
+};
+
+const part2 = () => {
+  const fileStr = readFileSync("./inputs/day3.txt").toString();
+
+  let result = 0;
+  let remainingStr = fileStr;
+  
+  while (remainingStr) {
+    const nextDontIndex = remainingStr.indexOf("don't()");
+
+    if (nextDontIndex < 0) {
+      result += sumValidProducts(remainingStr);
+
+      remainingStr = "";
+    } else {
+      const enabledStr = remainingStr.substring(0, nextDontIndex);
+
+      result += sumValidProducts(enabledStr);
+
+      const nextDoIndex = remainingStr.indexOf("do()", nextDontIndex);
+     
+      if (nextDoIndex > -1) {
+        remainingStr = remainingStr.substring(nextDoIndex);
+      } else {
+        remainingStr = "";
+      }
+    }
+  }
+
+  return result;
 };
 
 console.log("Part 1: " + part1()); // 187194524
+console.log("Part 2: " + part2()); // 127092535
