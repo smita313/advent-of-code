eef8913e3410ed8a548d6b4addbb258768be2f71
Smita Patankar <smita.patankar.313@gmail.com>
2024-12-04 03:26:32 -0500

    Day 4 Part 2

--- 2024/day4.ts
+++ 2024/day4.ts
@@ -2,6 +2,17 @@ import { readFileSync } from "fs";
 
 type Direction = { X: number; Y: number };
 
+const directions = [
+  { X: -1, Y: -1 },
+  { X: 0, Y: -1 },
+  { X: 1, Y: -1 },
+  { X: -1, Y: 1 },
+  { X: 0, Y: 1 },
+  { X: 1, Y: 1 },
+  { X: -1, Y: 0 },
+  { X: 1, Y: 0 },
+];
+
 const searchInDirection = (
   currCol: number,
   currRow: number,
@@ -40,17 +51,6 @@ const part1 = () => {
   const fileStr = readFileSync("./inputs/day4.txt").toString();
   const lines = fileStr.split("\n");
 
-  const directions = [
-    { X: -1, Y: -1 },
-    { X: 0, Y: -1 },
-    { X: 1, Y: -1 },
-    { X: -1, Y: 1 },
-    { X: 0, Y: 1 },
-    { X: 1, Y: 1 },
-    { X: -1, Y: 0 },
-    { X: 1, Y: 0 },
-  ];
-
   let total = 0;
   for (let currRow = 0; currRow < lines.length; currRow++) {
     for (let currCol = 0; currCol < lines[currRow].length; currCol++) {
@@ -63,4 +63,46 @@ const part1 = () => {
   return total;
 };
 
+const isCrossMas = (currCol: number, currRow: number, lines: string[]) => {
+  if (
+    lines[currRow][currCol] != "A" ||
+    currCol - 1 < 0 ||
+    currCol + 1 >= lines[currRow].length ||
+    currRow - 1 < 0 ||
+    currRow + 1 >= lines.length
+  ) {
+    return false;
+  }
+
+  const firstOption =
+    lines[currRow - 1][currCol - 1] + lines[currRow + 1][currCol + 1];
+  const secondOption =
+    lines[currRow - 1][currCol + 1] + lines[currRow + 1][currCol - 1];
+
+  return (
+    firstOption.includes("M") &&
+    firstOption.includes("S") &&
+    secondOption.includes("M") &&
+    secondOption.includes("S")
+  );
+};
+
+const part2 = () => {
+  const fileStr = readFileSync("./inputs/day4.txt").toString();
+
+  const lines = fileStr.split("\n");
+
+  let total = 0;
+  for (let currRow = 0; currRow < lines.length; currRow++) {
+    for (let currCol = 0; currCol < lines[currRow].length; currCol++) {
+      if (isCrossMas(currCol, currRow, lines)) {
+        total++;
+      }
+    }
+  }
+
+  return total;
+};
+
 console.log("Part 1: " + part1()); // 2613
+console.log("Part 2: " + part2()); // 1905
