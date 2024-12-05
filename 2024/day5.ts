ab7dbeac6fb472c172f23b4648899e34a6a2ba8e
Smita Patankar <smita.patankar@letshighlight.com>
2024-12-05 12:55:12 -0500

    Day 5 Part 2

--- 2024/day5.ts
+++ 2024/day5.ts
@@ -1,5 +1,20 @@
 import { readFileSync } from "fs";
 
+const checkPairOrder = (
+  orderingRules: string[][],
+  first: string,
+  second: string
+) => {
+  const filteredRule = orderingRules.find(
+    (rule) => rule.includes(first) && rule.includes(second)
+  );
+  if (filteredRule) {
+    return filteredRule[0] == first;
+  }
+
+  return true;
+};
+
 const part1 = () => {
   const fileStr = readFileSync("./inputs/day5.txt").toString();
 
@@ -7,24 +22,15 @@ const part1 = () => {
 
   const orderingRules = fileParts[0].split("\n").map((rule) => rule.split("|"));
 
-  const checkPairOrder = (first: string, second: string) => {
-    const filteredRule = orderingRules.find(
-      (rule) => rule.includes(first) && rule.includes(second)
-    );
-    if (filteredRule) {
-      return filteredRule[0] == first;
-    }
-
-    return true;
-  };
-
   const validUpdates = fileParts[1]
     .split("\n")
     .filter((s) => s)
     .map((update) => update.split(","))
     .filter((update) =>
       update.every((page, i, pages) =>
-        pages.slice(i + 1).every((nextPage) => checkPairOrder(page, nextPage))
+        pages
+          .slice(i + 1)
+          .every((nextPage) => checkPairOrder(orderingRules, page, nextPage))
       )
     );
 
@@ -33,4 +39,33 @@ const part1 = () => {
     .reduce((sum, current) => sum + current, 0);
 };
 
+const part2 = () => {
+  const fileStr = readFileSync("./inputs/day5.txt").toString();
+
+  const fileParts = fileStr.split("\n\n");
+
+  const orderingRules = fileParts[0].split("\n").map((rule) => rule.split("|"));
+
+  const sortedInvalidUpdates = fileParts[1]
+    .split("\n")
+    .filter((s) => s)
+    .map((update) => update.split(","))
+    .filter(
+      (update) =>
+        !update.every((page, i, pages) =>
+          pages
+            .slice(i + 1)
+            .every((nextPage) => checkPairOrder(orderingRules, page, nextPage))
+        )
+    )
+    .map((update) =>
+      update.sort((a, b) => (checkPairOrder(orderingRules, a, b) ? -1 : 1))
+    );
+
+  return sortedInvalidUpdates
+    .map((update) => parseInt(update[Math.floor(update.length / 2)]))
+    .reduce((sum, current) => sum + current, 0);
+};
+
 console.log("Part 1: " + part1()); // 4281
+console.log("Part 2: " + part2()); // 5466
