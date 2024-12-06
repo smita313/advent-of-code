97f25458c51f323f46ebec63d2b827662ff8f3b8
Smita Patankar <smita.patankar@letshighlight.com>
2024-12-06 14:48:00 -0500

    Day 6 Part 2 Brute Force

--- 2024/day6.ts
+++ 2024/day6.ts
@@ -42,4 +42,75 @@ const part1 = () => {
   return traversed.size;
 };
 
+const replaceAt = (str: string, index: number, replacement: string) => {
+  return (
+    str.substring(0, index) +
+    replacement +
+    str.substring(index + replacement.length)
+  );
+};
+
+const isInfiniteTraversal = (grid: string[]) => {
+  let currRow = grid.findIndex((row) => row.includes("^"));
+  let currCol = grid[currRow].indexOf("^");
+  let currDirectionIndex = 0;
+
+  let traversed: string[] = [];
+  while (
+    currRow > -1 &&
+    currRow < grid.length &&
+    currCol > -1 &&
+    currCol < grid[0].length
+  ) {
+    const entry = `${currCol},${currRow},${currDirectionIndex}`;
+
+    if (traversed.includes(entry)) {
+      return true;
+    }
+
+    traversed.push(`${currCol},${currRow},${currDirectionIndex}`);
+
+    const currDirection = traversalDirections[currDirectionIndex];
+
+    if (
+      grid.at(currRow + currDirection.Y)?.at(currCol + currDirection.X) != "#"
+    ) {
+      currRow += currDirection.Y;
+      currCol += currDirection.X;
+    } else {
+      currDirectionIndex = (currDirectionIndex + 1) % 4;
+    }
+  }
+
+  return false;
+};
+
+const part2 = () => {
+  const startTime = performance.now();
+
+  const fileStr = readFileSync("./inputs/day6.txt").toString();
+
+  const grid = fileStr.split("\n");
+
+  let total = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      const newGrid = [...grid];
+      if (newGrid[i][j] == ".") {
+        newGrid[i] = replaceAt(newGrid[i], j, "#");
+        if (isInfiniteTraversal(newGrid)) {
+          total++;
+        }
+      }
+    }
+  }
+
+  const endTime = performance.now();
+
+  console.log(`Part 2 took ${endTime - startTime} ms`);
+
+  return total;
+};
+
 console.log("Part 1: " + part1()); // 5030
+console.log("Part 2: " + part2()); // 1928
