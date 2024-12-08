07353bf1c95112bfc16080650b6575a3f512c755
Smita Patankar <smita.patankar.313@gmail.com>
2024-12-08 16:01:25 -0500

    Genericize utils function and add new index checker

--- 2024/utils.ts
+++ 2024/utils.ts
@@ -1,5 +1,13 @@
 export type Direction = { X: number; Y: number };
 
-export const isValidCoord = (grid: string[], row: number, col: number) => {
+export const isValidCoord = (
+  grid: any[][] | string[],
+  row: number,
+  col: number
+) => {
   return row >= 0 && row < grid.length && col >= 0 && col < grid[0].length;
+};
+
+export const isValidIndex = (arr: any[] | string, index: number) => {
+  return index >= 0 && index < arr.length;
 };
