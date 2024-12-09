d7513ec47545c57660a73d03d0f6d99b03a40170
Smita Patankar <smita.patankar.313@gmail.com>
2024-12-09 03:37:04 -0500

    Day 9 Part 2

--- 2024/day9.ts
+++ 2024/day9.ts
@@ -38,4 +38,56 @@ const part1 = () => {
   );
 };
 
-console.log("Part 1: " + part1());
+const part2 = () => {
+  const inputStr = readFileSync("./inputs/day9.txt").toString().trim();
+
+  type Block = { Id: number | undefined; Count: number };
+
+  const fileStructure: Block[] = inputStr.split("").map((digit, i) => {
+    if (i % 2 == 0) {
+      return { Id: i / 2, Count: parseInt(digit) };
+    }
+
+    return { Id: undefined, Count: parseInt(digit) };
+  });
+
+  const cleanedFiles = [...fileStructure];
+
+  for (let i = fileStructure.length - 1; i > -1; i--) {
+    const lastBlock = fileStructure[i];
+    if (lastBlock.Id != undefined) {
+      const availableSpot = cleanedFiles.findIndex(
+        (b) => b.Id == undefined && b.Count >= lastBlock.Count
+      );
+      const currIndex = cleanedFiles.findIndex((b) => b.Id == lastBlock.Id);
+
+      if (availableSpot != -1 && currIndex > availableSpot) {
+        const freeSpaceReplacement = {
+          Id: undefined,
+          Count: lastBlock.Count,
+        };
+
+        const relocatedReplacement = [
+          lastBlock,
+          {
+            Id: undefined,
+            Count: cleanedFiles[availableSpot].Count - lastBlock.Count,
+          },
+        ];
+        cleanedFiles.splice(currIndex, 1, freeSpaceReplacement);
+        cleanedFiles.splice(availableSpot, 1, ...relocatedReplacement);
+      }
+    }
+  }
+
+  return cleanedFiles
+    .filter((block) => block.Count > 0)
+    .flatMap((block) => Array<number | undefined>(block.Count).fill(block.Id))
+    .reduce(
+      (sum: number, current, position) => sum + position * (current ?? 0),
+      0
+    );
+};
+
+console.log("Part 1: " + part1()); // 6154342787400
+console.log("Part 2: " + part2()); // 6183632723350
