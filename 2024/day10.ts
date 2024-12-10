ea252142714b95fae465e5df489b2fe4541e83ff
Smita Patankar <smita.patankar.313@gmail.com>
2024-12-10 02:57:44 -0500

    Day 10 Part 2

--- 2024/day10.ts
+++ 2024/day10.ts
@@ -8,7 +8,7 @@ const validDirections: Direction[] = [
   { X: -1, Y: 0 },
 ];
 
-const findTrailheads = (
+const findUniqueTrailheads = (
   map: string[],
   nextHeight: number,
   currentRow: number,
@@ -25,19 +25,47 @@ const findTrailheads = (
             currentCol + validDirections[i].X
           }`
         );
+      } else {
+        findUniqueTrailheads(
+          map,
+          nextHeight + 1,
+          nextRow,
+          nextCol,
+          currentTrailheads
+        ).forEach((t) => currentTrailheads.add(t));
       }
-
-      findTrailheads(
-        map,
-        nextHeight + 1,
-        nextRow,
-        nextCol,
-        currentTrailheads
-      ).forEach((t) => currentTrailheads.add(t));
     }
   }
 
   return currentTrailheads;
+};
+
+const findTrailheadCount = (
+  map: string[],
+  nextHeight: number,
+  currentRow: number,
+  currentCol: number,
+  currentTotal: number
+): number => {
+  for (let i = 0; i < validDirections.length; i++) {
+    const nextRow = currentRow + validDirections[i].Y;
+    const nextCol = currentCol + validDirections[i].X;
+    if (checkGridElement(map, nextHeight.toString(), nextRow, nextCol)) {
+      if (nextHeight == 9) {
+        currentTotal = currentTotal + 1;
+      } else {
+        currentTotal = findTrailheadCount(
+          map,
+          nextHeight + 1,
+          nextRow,
+          nextCol,
+          currentTotal
+        );
+      }
+    }
+  }
+
+  return currentTotal;
 };
 
 const part1 = () => {
@@ -49,7 +77,23 @@ const part1 = () => {
   for (let row = 0; row < map.length; row++) {
     for (let col = 0; col < map[row].length; col++) {
       if (map[row][col] == "0") {
-        total += findTrailheads(map, 1, row, col, new Set<string>()).size;
+        total += findUniqueTrailheads(map, 1, row, col, new Set<string>()).size;
+      }
+    }
+  }
+
+  return total;
+};
+const part2 = () => {
+  const inputStr = readFileSync("./inputs/day10.txt").toString().trim();
+
+  const map = inputStr.split("\n");
+
+  let total = 0;
+  for (let row = 0; row < map.length; row++) {
+    for (let col = 0; col < map[row].length; col++) {
+      if (map[row][col] == "0") {
+        total += findTrailheadCount(map, 1, row, col, 0);
       }
     }
   }
@@ -58,3 +102,4 @@ const part1 = () => {
 };
 
 console.log("Part 1: " + part1()); // 776
+console.log("Part 2: " + part2()); // 1657
