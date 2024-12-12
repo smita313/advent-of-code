d781a54fba26567b455c640e326508e503256896
Smita Patankar <smita.patankar@letshighlight.com>
2024-12-12 18:12:44 -0500

    Working Part 1

--- 2024/day12.ts
+++ 2024/day12.ts
@@ -8,85 +8,86 @@ const searchDirections: Direction[] = [
   { X: -1, Y: 0 },
 ];
 
-const isAdjacentTo = (region: Coord[], point: Coord) => {
-  return region.some((r) =>
-    searchDirections.some((d) => point.X - r.X == d.X && point.Y - r.Y == d.Y)
-  );
-};
-
-const findHoles = (region: Coord[]) => {
-  const xVals = region
-    .map((c) => c.X)
-    .sort()
-    .slice(1, -1);
-  const yVals = region
-    .map((c) => c.Y)
-    .sort()
-    .slice(1, -1);
-
-  let holes: Coord[] = [];
-
-  for (let y = yVals[0]; y <= yVals[-1]; y++) {
-    for (let x = xVals[0]; x <= xVals[-1]; x++) {}
-  }
-  console.log({ holes });
-
-  return holes;
-};
-
-// todo: handle holes
-const calculatePerimeter = (region: Coord[]) => {
-  return (
-    new Set(region.map((c) => c.X)).size * 2 +
-    new Set(region.map((c) => c.Y)).size * 2 +
-    findHoles(region).length * 4
-  );
-};
-
-const part1 = () => {
-  //   const inputStr = readFileSync("./inputs/day12.txt").toString().trim();
-
-  const inputStr = `OOOOO
-OXOXO
-OOOOO
-OXOXO
-OOOOO`;
-
-  const regionMap = inputStr.split("\n");
-
+const findRegions = (grid: string[][]) => {
   const discoveredRegions: Record<string, Coord[]> = {};
 
-  for (let i = 0; i < regionMap.length; i++) {
-    for (let j = 0; j < regionMap.length; j++) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
       const coord: Coord = { X: j, Y: i };
 
       const existingKeys = Object.keys(discoveredRegions).filter((k) =>
-        k.includes(regionMap[i][j])
+        k.includes(grid[i][j])
       );
       if (existingKeys.length > 0) {
-        const adjacentRegionKey = existingKeys.find((k) =>
+        const adjacentRegionKeys = existingKeys.filter((k) =>
           isAdjacentTo(discoveredRegions[k], coord)
         );
 
-        if (adjacentRegionKey) {
-          discoveredRegions[adjacentRegionKey].push(coord);
+        if (adjacentRegionKeys.length == 1) {
+          discoveredRegions[adjacentRegionKeys[0]].push(coord);
+        } else if (adjacentRegionKeys.length > 1) {
+          adjacentRegionKeys.slice(1).forEach((k) => {
+            discoveredRegions[adjacentRegionKeys[0]].push(
+              ...discoveredRegions[k]
+            );
+
+            discoveredRegions[k] = [];
+          });
+
+          discoveredRegions[adjacentRegionKeys[0]].push(coord);
         } else {
-          discoveredRegions[`${regionMap[i][j]} ${existingKeys.length}`] = [
-            coord,
-          ];
+          discoveredRegions[`${grid[i][j]} ${existingKeys.length}`] = [coord];
         }
       } else {
-        discoveredRegions[`${regionMap[i][j]} 0`] = [coord];
+        discoveredRegions[`${grid[i][j]} 0`] = [coord];
       }
     }
   }
 
-  console.log(discoveredRegions);
+  return discoveredRegions;
+};
 
-  return Object.values(discoveredRegions).reduce(
-    (sum, current) => sum + calculatePerimeter(current) * current.length,
-    0
+const isAdjacentTo = (region: Coord[], point: Coord) => {
+  return region.some((r) =>
+    searchDirections.some((d) => r.X == point.X + d.X && r.Y == point.Y + d.Y)
   );
 };
 
-console.log("Part 1: " + part1());
+const calculatePerimeter = (region: Coord[]) => {
+  let total = 0;
+  for (let i = 0; i < region.length; i++) {
+    for (let d = 0; d < searchDirections.length; d++) {
+      if (
+        !region.some(
+          (regionElement) =>
+            regionElement.X == region[i].X + searchDirections[d].X &&
+            regionElement.Y == region[i].Y + searchDirections[d].Y
+        )
+      ) {
+        total++;
+      }
+    }
+  }
+
+  return total;
+};
+
+const part1 = () => {
+  const inputStr = readFileSync("./inputs/day12.txt").toString().trim();
+
+  const regionMap = inputStr.split("\n").map((row) => row.trim().split(""));
+
+  const discoveredRegions = findRegions(regionMap);
+
+  return Object.values(discoveredRegions).reduce((sum, current) => {
+    return sum + calculatePerimeter(current) * current.length;
+  }, 0);
+};
+
+const p1Start = performance.now();
+
+console.log("Part 1: " + part1()); // 1451030
+
+const p1End = performance.now();
+
+console.log(`Part 1 took ${p1End - p1Start} ms`);
